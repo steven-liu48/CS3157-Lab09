eceb44bb3b34a615169184a6af15c8cf4a7fe67e
Steven Liu <xl2948@columbia.edu>
2020-04-30 03:54:20 -0400

    Added test5

---
+++ test5.cpp
@@ -0,0 +1,34 @@
+// test5.cpp
+
+#include "mystring.h"
+#include <cassert>
+
+int main(){
+	cout << "2(a)" << endl;
+	MyString x("asdfds");
+	MyString y("bfdsafd");
+	assert((x<y) == 1);
+	assert((x>y) == 0);
+	assert((x==y) == 0);
+	assert((x!=y) == 1);
+	assert((x<=y) == 1);
+	assert((x>=y) == 0);
+	assert((x<"asdfdt") == 1);
+	assert(("asdfdt"<x) == 0);
+	
+	cout << "2(b)" << endl;
+	MyString s("hello"); 
+	s += " world";
+	cout << s << endl;
+	/*
+	// test op+=() and op+()
+	MyString sp(" "); 
+	MyString period("."); 
+	MyString str;
+	str += "This" + sp + "should" + sp += "work" + sp + "without"
+	+= sp + "any" + sp + "memory" += sp + "leak"
+	+= period;
+	cout << str << endl;
+	*/
+	return 0;
+}
